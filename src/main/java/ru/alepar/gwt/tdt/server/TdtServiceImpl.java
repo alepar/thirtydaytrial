47dc22f486e64bbd15c01330e5aeba782fd34126
alepar <alepar@gmail.com>
2010-07-10 17:10:25 +0400

    gae logging through slf4j

--- src/main/java/ru/alepar/gwt/tdt/server/TdtServiceImpl.java
+++ src/main/java/ru/alepar/gwt/tdt/server/TdtServiceImpl.java
@@ -1,11 +1,15 @@
 package ru.alepar.gwt.tdt.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.alepar.gwt.tdt.client.TdtService;
 
 public class TdtServiceImpl extends RemoteServiceServlet implements TdtService {
     // Implementation of sample interface method
     public String getMessage(String msg) {
+        Logger logger = LoggerFactory.getLogger(TdtServiceImpl.class);
+        logger.info("Client pinged me!");
         return "Client said: \"" + msg + "\"<br>Server answered: \"Hi!\"";
     }
 }
