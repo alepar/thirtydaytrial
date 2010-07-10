bda9cfc3fc02612b8e2eabaf312ff9dc24a4ffe3
alepar <alepar@gmail.com>
2010-07-10 18:14:48 +0400

    used User Authorization API

--- src/main/java/ru/alepar/gwt/tdt/server/TdtServiceImpl.java
+++ src/main/java/ru/alepar/gwt/tdt/server/TdtServiceImpl.java
@@ -1,15 +1,36 @@
 package ru.alepar.gwt.tdt.server;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.alepar.gwt.tdt.client.TdtService;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class TdtServiceImpl extends RemoteServiceServlet implements TdtService {
     // Implementation of sample interface method
     public String getMessage(String msg) {
+        final HttpServletRequest request = getThreadLocalRequest();
+        UserService userService = UserServiceFactory.getUserService();
+
+        String thisURL = "/";
+        String answer;
+        if (request.getUserPrincipal() != null) {
+            answer = "<p>Hello, " +
+                    request.getUserPrincipal().getName() +
+                    "!  You can <a href=\"" +
+                    userService.createLogoutURL(thisURL) +
+                    "\">sign out</a>.</p>";
+        } else {
+            answer = "<p>Please <a href=\"" +
+                    userService.createLoginURL(thisURL) +
+                    "\">sign in</a>.</p>";
+        }
+
         Logger logger = LoggerFactory.getLogger(TdtServiceImpl.class);
-        logger.info("Client pinged me!");
-        return "Client said: \"" + msg + "\"<br>Server answered: \"Hi!\"";
+        logger.info(answer);
+        return "Client said: \"" + msg + "\"<br>Server answered: " + answer;
     }
 }
