c28d972ad263be757c171c3938b0580d2053389b
alepar <alepar@gmail.com>
2010-07-11 17:39:39 +0400

    updated gwt-incubator lib with sources

---
+++ src/main/java/ru/alepar/gwt/tdt/client/presenter/TrialsTable.java
@@ -0,0 +1,9 @@
+package ru.alepar.gwt.tdt.client.presenter;
+
+/**
+ * User: alepar
+ * Date: Jul 11, 2010
+ * Time: 4:39:26 PM
+ */
+public class TrialsTable {
+}
---
+++ src/main/java/ru/alepar/gwt/tdt/client/view/TrialsTableDisplay.java
@@ -0,0 +1,9 @@
+package ru.alepar.gwt.tdt.client.view;
+
+/**
+ * User: alepar
+ * Date: Jul 11, 2010
+ * Time: 4:39:02 PM
+ */
+public class TrialsTableDisplay {
+}
