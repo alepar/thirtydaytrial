8b580a7af6b1eed79b1420ffad15cdbb6afddf1d
alepar <alepar@gmail.com>
2010-07-15 00:32:57 +0400

    unit test integration check

---
+++ src/test/java/ru/alepar/tdt/backend/jdo/dao/UnitTest.java
@@ -0,0 +1,17 @@
+package ru.alepar.tdt.backend.jdo.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * User: alepar
+ * Date: Jul 15, 2010
+ * Time: 12:32:02 AM
+ */
+public class UnitTest {
+
+    @Test
+    public void testSmth() {
+        Assert.fail();
+    }
+}
