222c58ab69526ecba40d6adb7ddf3ef191eb1fa4
alepar <alepar@gmail.com>
2010-07-22 07:33:52 +0400

    stricter bounds in MapTo

--- src/main/java/ru/alepar/tdt/backend/action/core/MapTo.java
+++ src/main/java/ru/alepar/tdt/backend/action/core/MapTo.java
@@ -9,5 +9,5 @@ import java.lang.annotation.RetentionPolicy;
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MapTo {
-    Class<?> value();
+    Class<? extends ActionHandler<?>> value();
 }
