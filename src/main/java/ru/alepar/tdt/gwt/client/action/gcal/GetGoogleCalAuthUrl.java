1335d94d01e9add3911b93d630dc9fc3bcac36db
alepar <alepar@gmail.com>
2010-08-09 00:40:02 +0400

    fixing build - wow, findbugs actually spotted real thing!

--- src/main/java/ru/alepar/tdt/gwt/client/action/gcal/GetGoogleCalAuthUrl.java
+++ src/main/java/ru/alepar/tdt/gwt/client/action/gcal/GetGoogleCalAuthUrl.java
@@ -1,5 +1,7 @@
 package ru.alepar.tdt.gwt.client.action.gcal;
 
+import ru.alepar.tdt.backend.action.core.MapTo;
+import ru.alepar.tdt.backend.action.gcal.GetGoogleCalAuthUrlHandler;
 import ru.alepar.tdt.gwt.client.action.core.TdtAction;
 import ru.alepar.tdt.gwt.client.action.core.TdtResponse;
 
@@ -8,11 +10,22 @@ import ru.alepar.tdt.gwt.client.action.core.TdtResponse;
  * Date: Aug 9, 2010
  * Time: 12:22:23 AM
  */
+@MapTo(GetGoogleCalAuthUrlHandler.class)
 public class GetGoogleCalAuthUrl implements TdtAction<GetGoogleCalAuthUrl.Response> {
 
-    public class Response implements TdtResponse {
+    public static class Response implements TdtResponse {
+
+        private String authUrl;
 
         public Response() { //used by serialization
+        }
+
+        public String getAuthUrl() {
+            return authUrl;
+        }
+
+        public void setAuthUrl(String authUrl) {
+            this.authUrl = authUrl;
         }
     }
 
